1c9615f6f6774cbba38cb254dee0a081ab79c70a
lidh04 <lidh04@gmail.com>
2023-03-31 07:38:47 +0800

    feat: implements searchTransaction

--- src/main/blocks.ts
+++ src/main/blocks.ts
@@ -4,23 +4,75 @@
  * @author lidh04
  * @license copyright to shc
  */
-import { mockTransData } from '../common/wallet-mock-data';
 import { TransVO, TxType } from "common/block-types";
 import { AddressType } from "common/wallet-types";
+import { store_get } from '../common/store-config';
+import axios from 'axios';
+
+type TransactionInfo = {
+  fromAddress: string;
+  toAddress: string;
+  txId: string;
+  idx: number;
+  txType: number; // txType: 0 积分, 1 所有权, 2身份认证
+  inOut: string;  // inOut代表输入还是输出: IN 输入, OUT 输出
+  value: string;
+  timestamp: string; // 产生时间 yyyy-mm-dd HH:MM:SS
+};
 
 export async function searchTransaction(address: string, addressType: AddressType, txType?: TxType, uuid?: string): Promise<TransVO[]> {
-  // TODO add search codes here
-  if (!address) {
-    if (txType === 0 || txType === 1 || txType === 2) {
-      return mockTransData.filter((t) => t.txType === txType);
-    } else if (uuid) {
-      return mockTransData.filter((t) => t.trans.indexOf(uuid) !== -1);
+  const host = store_get("host");
+  const url = `${host}/wallet/public/v1/search-transaction`;
+  const data = {
+    address,
+    inOut: addressType == "from" ? "OUT" : "IN",
+    transactionContent: {
+      type: typeof (txType) === 'undefined' ? 0 : txType,
+      uuid: ""
     }
-    return mockTransData;
+  }
+  if (!!uuid) {
+    data.inOut = "";
+    data.transactionContent.uuid = uuid;
+  }
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.status === 200) {
+      const json = response.data as { status: number, data: TransactionInfo[] };
+      if (json.status === 200) {
+        const data = json.data as TransactionInfo[];
+        console.log("receive data from url:", url, "data:", data);
+        return data.map(t => ({
+          from: t.fromAddress,
+          to: t.toAddress,
+          txType: t.txType,
+          trans: buildTrans(t),
+          timestamp: t.timestamp
+        }));
+      }
+    }
+  } catch (err: any) {
+    console.error("cannot post to url", url, "with data:", data, "error:", err.name, err.message, err);
   }
 
-  const filteredTransData = mockTransData.filter((t) => addressType === 'from' ? t.from === address : t.to === address);
-  return filteredTransData;
+  return [];
+}
+
+function buildTrans(t: TransactionInfo) {
+  switch (t.txType) {
+    case 0:
+      return `CRY: ${t.value}`;
+    case 1:
+      return `OWN: ${t.value}`;
+    case 2:
+      return `IDT: ${t.value}`;
+    default:
+      return "";
+  }
 }
 
 export async function sendTo(from: string, toEmail: string, assetType: number, value: number | string) {
