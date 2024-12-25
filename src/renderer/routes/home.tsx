a93eb2e7f02059b984a1bfbeaf6faa7ef4be3ced
dhoegreet <dhoegreet@gmail.com>
2024-12-25 16:25:34 +0800

    feat: modify the home pages.

--- src/renderer/routes/home.tsx
+++ src/renderer/routes/home.tsx
@@ -44,80 +44,72 @@ export default function Home(props: Props) {
         </p>
         <ul>
           <li>1. circle coin</li>
-          <li>2. ownership uid</li>
-          <li>3. idenity uid</li>
+          <li>2. ownership asset</li>
+          <li>3. identity asset</li>
         </ul>
 
-        <h2>What's the difference?</h2>
+        <h2>What is the difference?</h2>
         <p>
           The circle coin is just the coin token used to exchange for virtual assets including ownership, identity,
           authority of usage etc. All the virtual assets will be exchanged by circle coin.
         </p>
         <p>
           The blockchain assets includes ownership and identity. The ownership is the certificate to own some virtual
-          asset in the blockchain. The ownership is the certificate to show your idenity in the blockchain.
+          asset in the blockchain. The ownership is the certificate to show your identity in the blockchain.
         </p>
         <p>
-          The ownership can be transfered from one address to the other address with some coin fees(10<i>li</i>). When
-          one ownership is transfered, the new ownership uid will be generated to the transferer. The transfer and
-          recevier are not contrained except for the same address. So you can use your own two different addresses: one
-          for the transfer, the other for the receiver, with some transfer coin fees, you can 'buy' some ownership
-          assets for you.
+          The ownership can be transferred from one address to the other address with some coin fees(10<i>li</i>). When
+          one ownership is transferred, the new ownership uid will be generated to the transfer. The transfer and
+          receiver are not constrained except for the same address. So you can use your own two different addresses: one
+          for the transfer, the other for the receiver, with some transfer coin fees, you can &apos;buy&apos; some
+          ownership assets for you.
         </p>
         <p>
-          The identity can also be transfered from one address to the other address with some coin fees(10<i>li</i>).
-          When one identity is transfered, the new identity uid will be generated to the transfer. The transfer and
+          The identity can also be transferred from one address to the other address with some coin fees(10<i>li</i>).
+          When one identity is transferred, the new identity uid will be generated to the transfer. The transfer and
           receiver are not constrained exception for the same address. So you can use your own two different addresses:
-          one for the transfer, the other for the receiver, with some transfer coin fees, you can 'buy' some identity
+          one for the transfer, the other for the receiver, with some transfer coin fees, you can &apos;buy&apos; some
           assets for you.
         </p>
         <h2>How to use?</h2>
-        <h3>Anonymous user</h3>
+        <h3>Register and Login</h3>
+        <p>You must register with your email and login the app, then you can use the local services of the app.</p>
+        <h3>Why register and login?</h3>
         <p>
-          Anonymous user can use wallet functions: create wallet, list wallet info, show transactions etc. All the data
-          are stored in local storage encrypted with high security algorithms.
+          The app links all your local wallet data by your email. So you must register your valid email and login the
+          app with the right email which was proved to be owned by you.
         </p>
-        <h4>Create wallet</h4>
         <p>
-          Anonymous user can create local wallet with public and private keys. The wallet data is saved in local
-          storaged encrypted with specific encryptions.
+          We also send the wallet addresses(which are free to be open) with your email to cloud server. So in our cloud
+          server, your addresses will be searched by your email. The function will be very convenient for people who
+          want to send you assets, he only remember your email, not remember your complicated addresses.
+        </p>
+        <h3>Functions</h3>
+        <h4>1. Create wallet</h4>
+        <p>
+          Logged user can create wallet by public and private keys. The wallet data is saved in local machine securely
+          encrypted with specific high security encryption.
         </p>
 
-        <h4>List wallet</h4>
+        <h4>2. List wallet</h4>
         <p>
-          Anonymous user can list his wallet information: wallet address, public key, balance, ownership and identity
+          Logged user can list his wallet information: wallet address, public key, balance, ownership and identity
           assets etc.
         </p>
 
-        <h4>Transactions</h4>
-        <p>Anonymous user can list recently transactions: income transactions and spent transactions</p>
+        <h4>3. Transactions</h4>
+        <p>Logged user can list recently transactions: income transactions and spent transactions</p>
 
-        <h3>Account user</h3>
-        <p>
-          Account user is the user who register in the app and then login the app. Account user will post the encrypted
-          wallet data which is ecrypted with high security algorithms to p2p and open source circle node data server.
-          Account user should not remember or write down the private key relevant contents. He only use his registered
-          email to enjoy all the blockchain services.
-        </p>
-        <h4>Create wallet</h4>
-        <p>
-          Account user can create cloud wallet with public and private keys. The wallet data is saved in the cloud
-          server encrypted with specific high security encryptions.
-        </p>
+        <h4>4. Payment</h4>
+        <p>Logged user can pay circle coin or assets to others by using payment function.</p>
 
-        <h4>List wallet</h4>
-        <p>
-          Account user can list his wallet information: wallet address, public key, balance, ownership and identity
-          assets etc.
-        </p>
-
-        <h4>Transactions</h4>
-        <p>Account user can list recently transactions: income transactions and spent transactions</p>
+        <h4>5. Mine block locally</h4>
+        <p>When you register and then logged in, you can mine your block locally in your machine.</p>
 
         <h4>How to register or login?</h4>
         <p>
-          Please click the following buttons: If you are new user, please click 'Signup' to register, if you are old
-          user, please click "Signin" to login.
+          Please click the following buttons: If you are new user, please click &apos;Signup&apos; to register, if you
+          user, please click &quot;Signin&quot; to login.
         </p>
 
         {!account && (
